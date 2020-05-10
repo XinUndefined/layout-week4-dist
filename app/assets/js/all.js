5a643a0809fa445749aaed1e1ca29e26898eede1
XinUndfined <z26319491z@yahoo.com.tw>
2020-05-11 00:54:09 +0800

    js class 修正

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,18 +4,18 @@ $(".ham-icon").click(function (e) {
 });
 
 // 隱藏所有的 li 內文
-$(".text").hide();
+$(".qa-text").hide();
 // 預設讓第一個 li 加上 active
-$(".qa-item").eq(0).addClass("active");
+$(".qa-item").eq(0).addClass("qa-active");
 // 預設讓第一個 li 內文 顯示
-$(".text").eq(0).show();
+$(".qa-text").eq(0).show();
 
 // 點擊 li
 $(".qa-item").click(function (e) {
   // 取消預設 event 事件
   e.preventDefault();
   // 加上 active 屬性，並把其他有 active 的移除
-  $(this).toggleClass("active").siblings().removeClass("active");
+  $(this).toggleClass("qa-active").siblings().removeClass("qa-active");
   // 讓自己的 li body 切換收合
   $(this).children().next().slideToggle();
   // 讓其他 li body 收合
