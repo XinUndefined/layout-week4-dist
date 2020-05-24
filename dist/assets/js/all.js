21c53454e2bd6df4d2db36ab3bd8ab062597d162
XinUndfined <z26319491z@yahoo.com.tw>
2020-05-25 00:11:16 +0800

    視差滾動

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(".ham-icon").click(function (e) {
   e.preventDefault();
   $(".list").toggleClass("active");
@@ -5,23 +7,22 @@ $(".ham-icon").click(function (e) {
 $(".blog-icon").click(function (e) {
   e.preventDefault();
   $(".blog-list").toggleClass("active");
-});
+}); // 隱藏所有的 li 內文
 
-// 隱藏所有的 li 內文
-$(".qa-text").hide();
-// 預設讓第一個 li 加上 active
-$(".qa-item").eq(0).addClass("qa-active");
-// 預設讓第一個 li 內文 顯示
-$(".qa-text").eq(0).show();
+$(".qa-text").hide(); // 預設讓第一個 li 加上 active
 
-// 點擊 li
+$(".qa-item").eq(0).addClass("qa-active"); // 預設讓第一個 li 內文 顯示
+
+$(".qa-text").eq(0).show(); // 點擊 li
+
 $(".qa-item").click(function (e) {
   // 取消預設 event 事件
-  e.preventDefault();
-  // 加上 active 屬性，並把其他有 active 的移除
-  $(this).toggleClass("qa-active").siblings().removeClass("qa-active");
-  // 讓自己的 li body 切換收合
-  $(this).children().next().slideToggle();
-  // 讓其他 li body 收合
+  e.preventDefault(); // 加上 active 屬性，並把其他有 active 的移除
+
+  $(this).toggleClass("qa-active").siblings().removeClass("qa-active"); // 讓自己的 li body 切換收合
+
+  $(this).children().next().slideToggle(); // 讓其他 li body 收合
+
   $(this).siblings().children().next().slideUp();
 });
+//# sourceMappingURL=all.js.map
